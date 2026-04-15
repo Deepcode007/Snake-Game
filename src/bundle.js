cb96d1e67a891d940f0ddf05feba426dcb37e551
Deep Mallick <deepmallick2361@gmail.com>
2026-04-16 00:00:19 +0530

    Made a red food, appear randomly on start

--- src/bundle.js
+++ src/bundle.js
@@ -6,6 +6,15 @@ var ctx = canvas.getContext("2d");
 ctx.fillStyle = "darkgreen";
 ctx.fillRect(snake[0].x, snake[0].y, grid_size, grid_size);
 var del = "DOWN";
+var food = {
+  x: 0,
+  y: 0
+};
+var food_where = () => {
+  food.x = Math.floor(Math.random() * 20) * grid_size;
+  food.y = Math.floor(Math.random() * 20) * grid_size;
+};
+food_where();
 var draw = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   if (del == "UP") {
@@ -17,7 +26,10 @@ var draw = () => {
   } else if (del == "LEFT") {
     snake[0].x -= grid_size;
   }
+  ctx.fillStyle = "darkgreen";
   ctx.fillRect(snake[0].x, snake[0].y, grid_size, grid_size);
+  ctx.fillStyle = "red";
+  ctx.fillRect(food.x, food.y, grid_size, grid_size);
 };
 setInterval(draw, 1000);
 document.addEventListener("keydown", (key) => {
--- src/index.ts
+++ src/index.ts
@@ -15,26 +15,45 @@ ctx.fillStyle = "darkgreen"; // colour of paint brush
 
 ctx.fillRect(snake[0]!.x, snake[0]!.y, grid_size, grid_size);
 let del = "DOWN";
+
+
+let food = {
+  x: 0,
+  y: 0
+};
+
+// generate between 0.000 to 19.999 and then floor;
+const food_where = () => {
+  food.x = Math.floor(Math.random() * 20) * grid_size;
+  food.y = Math.floor(Math.random() * 20) * grid_size;
+};
+
+food_where();
+
 const draw = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   if (del == "UP")
   {
-    snake[0].y -= grid_size;
+    snake[0]!.y -= grid_size;
   }
   else if (del == "DOWN")
   {
-    snake[0].y += grid_size;
+    snake[0]!.y += grid_size;
   }
   else if (del == "RIGHT")
   {
-    snake[0].x += grid_size;
+    snake[0]!.x += grid_size;
   }
   else if (del == "LEFT")
   {
-    snake[0].x -= grid_size;
+    snake[0]!.x -= grid_size;
   }
+  ctx.fillStyle = "darkgreen";
+  
   ctx.fillRect(snake[0]!.x, snake[0]!.y, grid_size, grid_size);
 
+  ctx.fillStyle = "red";
+  ctx.fillRect(food.x, food.y, grid_size, grid_size);
 
 }
 
